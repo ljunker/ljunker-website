f54e073212887f939207b34477247534091ba205
Lars Junker <larsjunker92@gmail.com>
2024-12-19 13:12:37 +0100

    dynamic loading of content

---
+++ dist/scripts/navBarActivator.js
@@ -0,0 +1,20 @@
+function navBarActivate() {
+    var currentPath = window.location.href;
+
+    // Get the element with id "nav"
+    var navElement = document.getElementById("nav");
+
+    if (navElement) {
+    // Get all <a> elements inside the "nav" element
+        var links = navElement.getElementsByTagName("a");
+
+        // Loop through each <a> element
+        for (var i = 0; i < links.length; i++) {
+            // Check if the href attribute matches the current URL
+            if (links[i].href === currentPath) {
+            // Add the "active" class to the matching <a> element
+            links[i].className += " active";
+            }
+        }
+    }
+}
